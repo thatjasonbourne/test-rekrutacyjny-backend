69a7df48a94b7ad9f083a8d94b9933c2da355202
Jason Bourne <thatjasonbourne@gmail.com>
2023-01-18 19:21:02 +0100

    Solve task 3

--- src/main/java/pl/infinite/recruitment/tasks/task3/XmlTextExtractingHandler.java
+++ src/main/java/pl/infinite/recruitment/tasks/task3/XmlTextExtractingHandler.java
@@ -1,5 +1,14 @@
 package pl.infinite.recruitment.tasks.task3;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -8,8 +17,27 @@ import java.io.InputStream;
  */
 public class XmlTextExtractingHandler implements IndexingHandler {
 
+	private final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
 	@Override
 	public void handle(InputStream content, DocumentIndexBuilder builder) {
-		// TODO
+		try {
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document doc = documentBuilder.parse(content);
+			readRecursively(doc, builder);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void readRecursively(Node node, DocumentIndexBuilder builder) {
+		if (node.getNodeType() == Node.TEXT_NODE && !node.getTextContent().trim().isBlank()) {
+			builder.addText(node.getTextContent());
+		} else {
+			NodeList children = node.getChildNodes();
+			for (int i = 0; i < children.getLength(); i++) {
+				readRecursively(children.item(i), builder);
+			}
+		}
 	}
 }
